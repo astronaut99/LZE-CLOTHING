236ba212502f2693b078572839710bea1dd99d55
Rakshit Kumar <rakshitkumar90@gmail.com>
2021-05-03 16:38:08 +0530

    Day 2

---
+++ src/pages/homepage/homepage.com.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.com';
+import MenuItem from '../../components/menu-item/menu-item.com';
+import './homepage.sty.scss';
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
+
+
+export default HomePage;
